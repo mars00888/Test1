c51d827e0fe7f861ded6493ccf3374e695119484
mars00888 <33460267+mars00888@users.noreply.github.com>
2017-11-07 18:04:34 +0100

    MutiiTest

--- PapaTest/main.cpp
+++ PapaTest/main.cpp
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <iostream>
 
-int main(int argc, char **argv)
+using namespace std;
+int main()
 {
-	printf("hello world\n");
-	return 0;
+    //int answer = 0; 
+    string text = "";
+   // char buchstabe;
+    
+    std::cout <<"Bist du die Mutter von Maris? " << std::endl;
+   // std::cin >> buchstabe;
+    getline(cin, text);
+
+if(text[0] == 'j' && text[1] == 'a')
+//if (text=="ja")
+    std::cout << " Du hast den tollsten Sohn der Welt " << std::endl;
+else if  (text[0] == 'n' && text[1] == 'e' && text[2] == 'i' && text[3] == 'n') 
+    std::cout << " Tut mir leid für dich :`( " << std::endl;
+else 
+    std::cout << " Bitte nur mit ja oder nein antworten" << std::endl; 
+    
+   return 0;
 }
